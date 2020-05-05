103e558e566c39af3d1b24daf64702322da989ef
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-05 15:29:19 +0900

    clean/ BattlesController をリファクタリング

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -5,52 +5,58 @@ class BattlesController
 
   # 戦闘イベント（交互に攻撃し合う）
   def battle(**params)
-    user = params[:user]
-    enemy = params[:enemy]
+    set_character(params)
 
     loop do
-      user.attack(enemy)
-      break if battle_end?(enemy)
-  
-      enemy.attack(user)
-      break if battle_end?(user)
+      @user.attack(@enemy)
+      break if battle_end?
+      @enemy.attack(@user)
+      break if battle_end?
     end
 
-    if battle_result(user)
-      reward = calculate_battle_reward(enemy)
-      puts <<~EOS
+    battle_judgement
+  end
 
-      #{enemy.name}は倒れた。
-      #{enemy.name}との戦闘に勝利した！
-      Exp #{reward[:exp]}
-      Gold #{reward[:gold]}
-      を獲得した！
+  private
 
-      EOS
-    else
-      puts <<~EOS
-
-      #{user.name}は倒れた。
-      #{enemy.name}との戦闘に敗北した...
-
-      EOS
+    def set_character(**params)
+      @user = params[:user]
+      @enemy = params[:enemy]
     end
-  end
 
-  # 戦闘終了フラグ
-  def battle_end?(target)
-    target.hp <= 0
-  end
+    # 戦闘終了フラグ
+    def battle_end?
+      @user.hp <= 0 || @enemy.hp <= 0
+    end
 
-  # 戦闘結果
-  def battle_result(user)
-    user.hp > 0
-  end
+    # 戦闘結果
+    def battle_judgement
+      if user_won?
+        reward = calculate_battle_reward
+        puts <<~EOS
+        #{@enemy.name}は倒れた。
+        #{@enemy.name}との戦闘に勝利した！
+        Exp #{reward[:exp]}
+        Gold #{reward[:gold]}
+        を獲得した！
+        EOS
+      else
+        puts <<~EOS
+        #{@user.name}は倒れた。
+        #{@enemy.name}との戦闘に敗北した...
+        EOS
+      end
+    end
 
-  # 戦闘報酬の計算
-  def calculate_battle_reward(enemy)
-    exp = (enemy.str + enemy.vit) * EXP_CONSTANT
-    gold = (enemy.max_hp) * GOLD_CONSTANT
-    reward = { exp: exp, gold: gold }
-  end
+    # 戦闘結果の判定
+    def user_won?
+      @user.hp > 0
+    end
+
+    # 戦闘報酬の計算
+    def calculate_battle_reward
+      exp = (@enemy.str + @enemy.vit) * EXP_CONSTANT
+      gold = (@enemy.max_hp) * GOLD_CONSTANT
+      reward = { exp: exp, gold: gold }
+    end
 end
