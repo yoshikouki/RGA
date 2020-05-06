779233e2026eee5df1e4f89350725c8e353f1377
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-06 22:51:43 +0900

    add/ セッション系のリダイレクトに関するメソッドを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ログイン後のリダイレクト先を定義
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  private
+
+    # 非ログインユーザーが、ログイン必須なアクセスをした場合の処理
+    def sign_in_required
+      redirect_to(new_user_session_url) unless user_signed_in?
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  # before_action :sign_in_required, only: [:index]
+
   def home
   end
 end
