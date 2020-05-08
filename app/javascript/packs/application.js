b6c6d6ce5b3193810d17b03483474c9a87368123
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-08 14:46:45 +0900

    fix/ Bootstrap の設定を修正。ディレクトリ名を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
-import '../stylesheets/application'
+import '../src/application'
