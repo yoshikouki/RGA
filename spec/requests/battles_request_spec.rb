03243acaa5caa7e2a5f556cf66ad3858b9233f76
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-10 17:31:13 +0900

    fix/ エラー対応。CircleCI 設定ファイルを修正

--- spec/requests/battles_request_spec.rb
+++ spec/requests/battles_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Battles', type: :request do
   describe 'GET /battle' do
-    # it 'returns http success' do
+    it 'returns http success'
     #   get '/battles/battle'
     #   expect(response).to have_http_status(:success)
     # end
