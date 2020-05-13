9a69d37aa1f55381952ffd982820a1022b371df9
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-13 12:11:33 +0900

    add/ View用の Bootstrap クラスセットリストを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_var
 
   # ログイン後のリダイレクト先を定義
   def after_sign_in_path_for(_resource)
@@ -19,4 +20,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def set_var
+    @bootstrap_class_list = BOOTSTRAP_CLASS_LIST
+  end
+
+  BOOTSTRAP_CLASS_LIST = {
+    v: ''
+  }.freeze
 end
