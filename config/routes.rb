c38eb5641a155995e2b9e4fb76b6defb1483f225
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-15 10:59:14 +0900

    update/ Devise Controller を作成。UserModelとPlayerModelのれん系のため
    Twitter認証用Controllerを移動。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users,
+             controllers: {
+               confirmations:      'users/confirmations',
+               passwords:          'users/passwords',
+               registrations:      'users/registrations',
+               sessions:           'users/sessions',
+               unlocks:            'users/unlocks',
+               omniauth_callbacks: 'users/omniauth_callbacks'
+             }
 
   get 'battles/battle'
   get 'battles', to: 'battles#index'
