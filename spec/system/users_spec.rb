8c3c1637e9983a10cf802d9b6a58bc5388b856bd
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-15 14:27:00 +0900

    update/ アカウント新規作成テストを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,20 +5,23 @@ RSpec.describe 'Users', type: :system do
     driven_by(:rack_test)
   end
 
-  it '新規アカウントが作成できる' do
-    visit root_path
-    within('header') do
-      click_on 'sign-up'
+  describe '#new, #create' do
+    let(:user) { FactoryBot.build(:user) }
+
+    it '新規アカウントが作成できる' do
+      visit root_path
+      within('header') { click_on 'sign-up' }
+      expect(page).to have_content '新規アカウント'
+      within('#sign-up-form') do
+        fill_in 'form-email', with: user.email
+        fill_in 'form-username', with: user.username
+        fill_in 'form-password', with: user.password
+        fill_in 'form-password-confirmation', with: user.password
+      end
+      expect { click_on 'form-submit' }.to \
+        change { User.count }.by(1)
+      expect(Player.first.name).to eq user.username
     end
-    expect(page).to have_content '新規アカウント'
-    within('#sign-up-form') do
-      fill_in 'form-email', with: 'example@email.com'
-      fill_in 'form-username', with: 'テストユーザー'
-      fill_in 'form-password', with: 'password'
-      fill_in 'form-password-confirmation', with: 'password'
     end
-    expect { click_on 'form-submit' }.to \
-      change { User.count }.from(0).to(1)
-    expect(Player.first.name).to eq 'テストユーザー'
   end
 end
