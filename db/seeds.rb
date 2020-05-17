14ccb8b0e3f7a2e6be9e7905c7cb98b07b905673
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-17 11:48:20 +0900

    fix/ seedsファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,12 @@ require 'csv'
 jobs = []
 CSV.foreach('db/data/jobs_master_data.csv', headers: true) do |row|
   params = row.to_h.transform_keys(&:to_s)
-  jobs << Job.build(params)
+  jobs << Job.create(params)
 end
-jobs.save
 
 # ユーザー
 User.create!(username:              'Game Master',
              email:                 'yoshikouki@gmail.com',
              password:              'password',
              password_confirmation: 'password',
-             confirmation_at:       Time.zone.now)
+             confirmed_at:          Time.zone.now)
