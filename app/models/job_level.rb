74567efef638668d437797371baf7ba3e422afb1
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-18 23:31:04 +0900

    fix/ ジョブレベルが上限を超えてレベルアップしてしまう不具合を修正
    レベルアップ上限の判断をレベルではなくEXPを基準で行うように修正
    レベルアップ経験値を計算するメソッドを追加（リファクタリング】

--- app/models/job_level.rb
+++ app/models/job_level.rb
@@ -5,22 +5,29 @@ class JobLevel < ApplicationRecord
 
   attr_accessor :job_level_up_diff
 
-  # ジョブがレベル上限に達していればtrue
-  def reached_in_level_limit?
-    job_level >= Job.find(job_id).level_limit
-  end
-
   # 獲得した経験値をジョブEXPに反映
   def earn_reward(reward)
     return self if reached_in_level_limit?
 
-    update(job_exp: self.job_exp += reward[:get_exp])
+    earned_exp = job_exp + reward[:get_exp]
+    limit_exp = calculate_exp_to_level_up(job.level_limit)
+    update(job_exp: earned_exp >= limit_exp ? limit_exp : earned_exp)
     self
+  end
+
+  # ジョブがレベル上限に達していればtrue
+  def reached_in_level_limit?
+    job_exp >= calculate_exp_to_level_up(job.level_limit)
+  end
+
+  # レベルアップするための経験値を計算
+  def calculate_exp_to_level_up(level)
+    level**NEXT_LV_EXP_DIFF
   end
 
   # ジョブレベルが上がっているか判定する
   def level_upped?
-    job_exp >= (job_level + 1)**2
+    job_exp >= calculate_exp_to_level_up(job_level + 1)
   end
 
   # ジョブレベルアップの処理
@@ -30,7 +37,7 @@ class JobLevel < ApplicationRecord
     next_lv_diff = 1
     loop do
       next_lv_diff += 1
-      break if job_exp < (job_level + next_lv_diff)**NEXT_LV_EXP_DIFF
+      break if job_exp < calculate_exp_to_level_up(job_level + next_lv_diff)
     end
     @job_level_up_diff = (next_lv_diff - 1)
     grow_job_status
