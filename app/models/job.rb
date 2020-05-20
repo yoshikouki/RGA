1bfe9e6fe41314313e82fbf584fcd4b96be105b2
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-20 16:22:53 +0900

    add/ Job Change Conditions Model を作成。依存関係を定義

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@
 class Job < ApplicationRecord
   # nullifi:JobModelの削除次、job_levels.job_idはnullになる
   has_many :job_levels, dependent: :nullify
+  has_many :job_change_conditions, dependent: :nullify
 end
---
+++ spec/factories/job_change_conditions.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :job_change_condition do
+    job_id { '' }
+    condition_job_id { '' }
+    condition_job_level { '' }
+  end
+end
