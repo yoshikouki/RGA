15a7c1a37327ab46f3c82010989c244c3c1a862b
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-25 17:22:34 +0900

    add/ スキルに関する仮マスタデータのSeedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,18 @@ CSV.foreach('db/data/job_change_conditions_master_data.csv', headers: true) do |
   JobChangeCondition.create(params)
 end
 
+# Skills マスタ
+CSV.foreach('db/data/skills_master_data.csv', headers: true) do |row|
+  params = row.to_h.transform_keys(&:to_s)
+  Skill.create(params)
+end
+
+# SkillAcquisitionConditions マスタ
+CSV.foreach('db/data/skill_acquisition_conditions_master_data.csv', headers: true) do |row|
+  params = row.to_h.transform_keys(&:to_s)
+  SkillAcquisitionCondition.create(params)
+end
+
 # ユーザー
 User.create!(username:              'Game Master',
              email:                 'yoshikouki@gmail.com',
